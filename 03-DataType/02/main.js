d9ce8b69f3321085da8dab03a24c5aff027389c5
Saowaluckpruitt <dolphinza8.pruit.mn@gmail.com>
2022-03-25 23:58:28 +0700

    js01 datatype

---
+++ 03-DataType/02/main.js
@@ -0,0 +1,10 @@
+const firstName = "Saowaluck";
+const lasttName = "Pruitt";
+const age = 30;
+const add = "123 bkk";
+console.log(
+  `Full name :${firstName},
+  ${lasttName},
+  Age :${age},
+  Add :${add}`
+);
